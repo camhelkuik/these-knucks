e267be7d3839ce6c45d42e74b6444b95a5842414
Cameron Helkuik <cameron@travefy.com>
2018-08-12 21:04:44 -0500

    have a random array working and a bit of color added

---
+++ app/controllers/knucks.js
@@ -0,0 +1,14 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+    knucks: null, 
+    knucksArray: ["your mama", "punk cats", "funk rats"],
+
+    actions : {
+        generateKnucks(){
+            var array = this.get('knucksArray');
+            var rand = array[Math.floor(Math.random() * array.length)];
+            this.set('knucks', rand);
+        }
+    }
+});
